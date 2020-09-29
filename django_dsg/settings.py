21049b8997fc1118a39271d8f5e4ceccc3a7ea80
MaksimEkin <meren1@umbc.edu>
2020-09-29 11:15:39 -0400

    Added env variable for ALLOWED HOSTS.

--- django_dsg/settings.py
+++ django_dsg/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 
-ALLOWED_HOSTS = ['data-structures-game.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('ALLOWED_HOSTS_1')]
 
 
 # Application definition
