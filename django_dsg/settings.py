9cfd1382eda5775f13db7997476234e574957400
MaksimEkin <meren1@umbc.edu>
2020-09-29 01:31:52 -0400

    Changed to allowed hosts.

--- django_dsg/settings.py
+++ django_dsg/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE') == "True")
 
-ALLOWED_HOSTS = ['https://data-structures-game.herokuapp.com']
+ALLOWED_HOSTS = ['data-structures-game.herokuapp.com']
 
 
 # Application definition
