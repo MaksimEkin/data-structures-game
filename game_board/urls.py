f24e2d8a1461ffb17043a1e0badecb0ef626980e
MaksimEkin <meren1@umbc.edu>
2020-10-06 17:55:19 -0400

    Adding example API calls.

--- game_board/urls.py
+++ game_board/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+    # API Calls
     path('api', views.api_overview, name='game-board-api_overview'),
-    path('api/start_game/<int:difficulty>', views.start_game, name='game-board-start_game'),
-    path('api/game_status/<int:game_id>', views.game_status, name='game-board-game_status')
+    path('api/start_game/<int:difficulty>/<int:num_players>/<str:online>', views.start_game, name='game-board-start_game'),
+    path('api/board/<int:game_id>', views.board, name='game-board-game_status'),
+    path('api/rebalance/<str:graph>', views.rebalance, name='game-board-rebalance'),
+    path('api/action/<str:card>', views.action, name='game-board-action')
 ]
--- game_board/views.py
+++ game_board/views.py
@@ -6,22 +6,36 @@ from django.http import JsonResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-from .database import game_board_db
+from .database import game_board_db as db
 
 @api_view(['GET'])
 def api_overview(request):
     api_urls = {
-        'Start Game' : '/start_game/<int:difficulty>',
-        'Game Status' : '/game_status/<int:id>'
+        'Start Game'      : '/start_game/<int:difficulty>/<int:num_players>/<bool:online>',
+        'Game Board'      : '/board/<int:id>',
+        'Re-balance Tree' : '/rebalance/<str:graph>',
+        'Action'          : '/action/<str:card>'
     }
     return Response(api_urls)
 
 
+@api_view(['GET', 'POST'])
+def start_game(request, difficulty, num_players, online):
+    sample = {
+        'to-do'      : 'Currently being implemented!',
+        'difficulty' : str(difficulty),
+        'num_players': str(num_players),
+        'online'     : bool(online),
+        'game_id'    : 8
+    }
+    return Response(sample)
+
+
 @api_view(['GET'])
-def game_status(request, game_id):
+def board(request, game_id):
 
     try:
-        game_state = game_board_db.read_game(int(game_id))
+        game_state = db.read_game(int(game_id))
     except Exception as e:
         return Response({'Error': str(e)})
 
@@ -29,19 +43,42 @@ def game_status(request, game_id):
         return Response({'Not Found!'})
 
     sample = {
-        'To-Do' : 'Currently being implemented!',
-        'Input' : str(game_id),
-        'Game' : game_state['game id'],
-        'Data Structure State' : game_state['game state'],
-        'Points' : game_state['points'],
-        'Turn' : game_state['player id current turn']
+        'to-do'       : 'Currently being implemented!',
+        'game_id'     : game_state['game id'],
+        'graph'       : game_state['game state'],
+        'player_ids'  : ['id2', 'id3', 'id4', 'id5'],
+        'player_names': ['naomi', 'kulsoom', 'nick', 'ryan'],
+        'points'      : {'id2':2, 'id3':2, 'id4':3, 'id5':10},
+        'turn'        : game_state['player id current turn'],
+        'cards'       : {'id2': ['card1','card2','card3'], 'id3': ['card1','card2','card3'], 'id4':['card1','card2','card3'], 'id5':['card1','card2','card3']},
+        'gold_node'   : False,
+        'difficulty'  : 'Medium',
+        'num_players' : 4,
+        'online'      : True
     }
     return Response(sample)
 
+
 @api_view(['GET', 'POST'])
-def start_game(request, difficulty):
+def rebalance(request, graph):
     sample = {
-        'To-Do' : 'Currently being implemented!',
-        'Input' : str(difficulty)
+        'to-Do'     : 'Currently being implemented!',
+        'graph'     : str(graph),
+        'correct': True
+    }
+    return Response(sample)
+
+
+@api_view(['GET', 'POST'])
+def action(request, card):
+    sample = {
+        'to-Do'      : 'Currently being implemented!',
+        'graph'      : "4(2(3)(1))(6(5))",
+        'played'     : str(card),
+        'new_card'   : 'card1',
+        'turn'       : 2,
+        'points'     : -20,
+        'balanced'   : True,
+        'golden_node': False
     }
     return Response(sample)
