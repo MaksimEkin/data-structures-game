a50a9e56cbea86defe225f53a89f39b7d9defcf7
MaksimEkin <meren1@umbc.edu>
2020-10-06 19:53:13 -0400

    Added more fields to the example API call return for the game board state.

--- game_board/views.py
+++ game_board/views.py
@@ -8,6 +8,9 @@ from rest_framework.response import Response
 
 from .database import game_board_db as db
 
+from datetime import datetime
+
+
 @api_view(['GET'])
 def api_overview(request):
     api_urls = {
@@ -43,18 +46,24 @@ def board(request, game_id):
         return Response({'Not Found!'})
 
     sample = {
-        'to-do'       : 'Currently being implemented!',
-        'game_id'     : game_state['game id'],
-        'graph'       : game_state['game state'],
-        'player_ids'  : ['id2', 'id3', 'id4', 'id5'],
-        'player_names': ['naomi', 'kulsoom', 'nick', 'ryan'],
-        'points'      : {'id2':2, 'id3':2, 'id4':3, 'id5':10},
-        'turn'        : game_state['player id current turn'],
-        'cards'       : {'id2': ['card1','card2','card3'], 'id3': ['card1','card2','card3'], 'id4':['card1','card2','card3'], 'id5':['card1','card2','card3']},
-        'gold_node'   : False,
-        'difficulty'  : 'Medium',
-        'num_players' : 4,
-        'online'      : True
+        'game_id'                 : str('id12dawda')+str(game_state['game id']),
+        'graph'                   : {'nodes':'node4(node2(node3)(node1))(node6(node5))', 'node_points':{'node1':1, 'node2':2,'node3':3, 'node4':4,'node5':5, 'node6':6}, 'gold_node':'node5', 'balanced':True},
+        'player_ids'              : ['id2', 'id3', 'id4', 'id5'],
+        'player_names'            : ['naomi', 'kulsoom', 'nick', 'ryan'],
+        'player_points'           : {'id2':2, 'id3':2, 'id4':3, 'id5':10},
+        'turn'                    : 'id2',
+        'cards'                   : {'id2': ['card1','card2','card3'], 'id3': ['card1','card2','card3'], 'id4':['card1','card2','card3'], 'id5':['card1','card2','card3']},
+        'gold_node'               : False,
+        'difficulty'              : 'Medium',
+        'num_players'             : 4,
+        'online'                  : True,
+        'curr_data_structure'     :'AVL',
+        'selected_data_structures': ['AVL', 'Stack'],
+        'timed_game'              : False,
+        'seconds_until_next_ds'   : 60,
+        'time_created'            : datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+        'end_game'                : False,
+
     }
     return Response(sample)
 
