b8df4554f3468cca35640c6b9fe0fd17e26909ea
MaksimEkin <meren1@umbc.edu>
2020-10-06 23:34:49 -0400

    Added functionality to the API calls to demo a game. Added a cheat check in utils. Added new function to create the game board dict in utils. Factored out the reading game status from db into utils.

--- game_board/utils.py
+++ game_board/utils.py
@@ -1,7 +1,113 @@
 from . import config
-import random
+from .database import game_board_db as db
 
-def generate_cards(player_ids, nodes, data_structure, cards_per, difficulty):
+from datetime import datetime
+from bson import json_util, ObjectId
+
+import random
+import uuid
+import json
+
+def load_board(game_id):
+    '''
+    Load a game board state from database.
+
+    :string game_id: id of the game board
+    :return: game board dict or error
+    '''
+    try:
+        # game_board = mock_db.read_game('game_id1234')
+        game_board = db.read_game(str(game_id))
+        if game_board == "nah bro idk about it":
+            return True, 'Game Not Found!'
+
+        # Serialize
+        game_board = json.loads(json_util.dumps(game_board))
+
+        # Remove the database entry ID
+        del game_board['_id']
+
+    except Exception as e:
+        return True, str(e)
+
+    return False, game_board
+
+
+def new_board(difficulty, player_ids, data_structures, online):
+    '''
+    Create new board dictionary.
+
+    :string difficulty: difficulty of the game
+    :list player_ids: list of player ids
+    :list data_structures: list of data structures
+    :string online: multiplater/online
+    :return: game board dict
+    '''
+
+    # Call Nick's AVL lib here
+    graph =  {'nodes': 'node4(node2(node3)(node1))(node6(node5))',
+              'node_points': {'node1': 1, 'node2': 2, 'node3': 3, 'node4': 4, 'node5': 5, 'node6': 6},
+              'gold_node': 'node5', 'balanced': True}
+
+    board = {
+        'game_id': str(uuid.uuid1()),
+        'graph': graph,
+        'player_ids': player_ids,
+        'player_names': [''],
+        'player_points': {str(id):0 for id in player_ids},
+        'turn': random.choice(player_ids),
+        'cards': generate_cards(player_ids, list(graph['node_points'].keys()), data_structures[0], difficulty),
+        'gold_node': False,
+        'difficulty': difficulty,
+        'num_players': len(player_ids),
+        'online': online,
+        'curr_data_structure': data_structures[0],
+        'selected_data_structures': data_structures,
+        'timed_game': False,
+        'seconds_until_next_ds': 60,
+        'time_created': datetime.now().strftime("%d/%m/%Y %H:%M:%S"),
+        'end_game': False
+    }
+
+    return board
+
+
+def cheat_check(game_board, card=-1, player_id=-1, rebalance=-1):
+    '''
+    Check if the action that is attempted is valid.
+
+    :dict game_board:
+    :string card:
+    :string player_id:
+    :bool rebalance:
+    :return: True if invalid action, and string for reason.
+    '''
+
+    # Check if the game have ended
+    if game_board['end_game'] == True:
+        return True, str('Game has finished!')
+
+    # Check if the user is in the game
+    if player_id != -1 and (player_id not in game_board['player_ids']):
+        return True, str('Player ' + str(player_id) + ' is not in the game!')
+
+    # Check if it is the given player's turn
+    if game_board['turn'] != player_id:
+        return True, str('Player ' + str(player_id) + ' can not play now!')
+
+    # Check if the user has the claimed card
+    if card != -1 and (card not in game_board['cards'][str(player_id)]):
+        return True, str('Player does not have the card ' + str(card) + '!')
+
+    # Check if the graph is in rebalance state
+    if rebalance != -1 and (game_board['graph']['balanced'] == True):
+        return True, str('Tree is already balanced!')
+
+    # No cheat detected
+    return False, ''
+
+
+def generate_cards(player_ids, nodes, data_structure, difficulty):
     '''
     Distribute cards to the users.
 
@@ -21,7 +127,7 @@ def generate_cards(player_ids, nodes, data_structure, cards_per, difficulty):
     # Pick cards for each player
     for player in player_ids:
         # cards (actions) for the current player
-        cards = random.choices(config.CARDS[str(data_structure)], k=int(cards_per))
+        cards = random.choices(config.CARDS[str(data_structure)], k=int(config.CARDS_PER_PLAYER))
         cards_= list()
 
         for card in cards:
--- game_board/views.py
+++ game_board/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 from django.http import JsonResponse
 
@@ -7,10 +6,11 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 from .database import game_board_db as db
-from .mock import MockDB as mock_db
 from . import utils
 
 from datetime import datetime
+import json
+from bson import json_util, ObjectId
 
 
 @api_view(['GET'])
@@ -18,8 +18,8 @@ def api_overview(request):
     api_urls = {
         'Start Game'      : '/start_game/<str:difficulty>/<str:player_ids>/<str:data_structures>/<bool:online>',
         'Game Board'      : '/board/<int:id>',
-        'Re-balance Tree' : '/rebalance/<str:graph>',
-        'Action'          : '/action/<str:card>'
+        'Re-balance Tree' : '/rebalance/<str:graph>/<str:player_id>/<str:game_id>',
+        'Action'          : '/action/<str:card>/<str:player_id>/<str:game_id>'
     }
     return Response(api_urls)
 
@@ -29,48 +29,71 @@ def start_game(request, difficulty, player_ids, data_structures, online):
 
     player_ids = player_ids.split(',')
     data_structures = data_structures.split(',')
-    nodes = ['node1', 'node2', 'node3']
 
-    Cards = utils.generate_cards(player_ids, nodes, data_structures[0], 3, difficulty)
-
-    sample = {
-        'to-do' : 'Currently being implemented!',
-        'Cards' : Cards,
-        'pick' : utils.new_card(data_structures[0], nodes, difficulty)
-    }
-    return Response(sample)
+    try:
+        game_id = db.create_game(utils.new_board(difficulty, player_ids, data_structures, online))
+        game_id = json.loads(json_util.dumps(game_id))
+        return Response({'game_id': game_id})
+    except Exception as e:
+        return Response({'error': str(e)})
 
 
 @api_view(['GET'])
 def board(request, game_id):
 
+    Error, board = utils.load_board(game_id)
+    if Error:
+        return Response({'error': board})
+
+    return Response(board)
+
+
+@api_view(['GET', 'POST'])
+def rebalance(request, graph, player_id, game_id):
+
+    Error, board = utils.load_board(game_id)
+    if Error:
+        return Response({'error': board})
+
+    cheat, reason = utils.cheat_check(game_board=board, player_id=player_id, rebalance=True)
+    if cheat:
+        return Response({'cheat_detected': reason})
+
+    # Do the rebalance action with Nick's AVL lib here
+    graph = {'nodes': 'node4(node2(node3)(node1))(node6))',
+             'node_points': {'node1': 1, 'node2': 2, 'node3': 3, 'node4': 4, 'node5': 5, 'node6': 6},
+             'gold_node': 'node5', 'balanced': True}
+    board['graph'] = graph
+
     try:
-        # game_board = db.read_game(int(game_id))
-        game_board = mock_db.read_game('game_id1234')
-
-        # Remove the database entry ID
-        del game_board['id_']
-
+        db_response = db.update_game(game_id, board)
     except Exception as e:
-        return Response({'Error': str(e)})
+        return Response({'error': str(e)})
 
-    if game_board == None:
-        return Response({'Not Found!'})
-
-    return Response(game_board)
+    return Response(board)
 
 
 @api_view(['GET', 'POST'])
-def rebalance(request, graph):
-    sample = {
-        'to-Do'     : 'Currently being implemented!'
-    }
-    return Response(sample)
+def action(request, card, player_id, game_id):
+
+    Error, board = utils.load_board(game_id)
+    if Error:
+        return Response({'error': board})
 
 
-@api_view(['GET', 'POST'])
-def action(request, card):
-    sample = {
-        'to-Do'      : 'Currently being implemented!'
-    }
-    return Response(sample)
+    cheat, reason = utils.cheat_check(game_board=board, card=card, player_id=player_id)
+    if cheat:
+        return Response({'cheat_detected': reason})
+
+    # Do the card action with Nick's AVL lib here
+    graph = {'nodes': 'node4(node2(node3)(node1))(node6))',
+             'node_points': {'node1': 1, 'node2': 2, 'node3': 3, 'node4': 4, 'node5': 5, 'node6': 6},
+             'gold_node': 'node5', 'balanced': False}
+    board['graph'] = graph
+
+    try:
+        db_response = db.update_game(game_id, board)
+    except Exception as e:
+        return Response({'error': str(e)})
+
+    return Response(board)
