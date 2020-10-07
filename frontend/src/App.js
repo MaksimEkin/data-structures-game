0e6193473fdbf5bd06d30e845b62f837ad002cbd
kulriz <42681721+kulriz@users.noreply.github.com>
2020-10-07 01:05:42 -0400

    added test lines to react page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,6 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
+          The little AVL tree that could! <br></br>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
