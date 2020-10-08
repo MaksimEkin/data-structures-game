fdd56907de7f2a23dadc065c87dc8a1dcc930146
MaksimEkin <meren1@umbc.edu>
2020-10-08 00:52:10 -0400

    Configuring the static directory for heroku.

--- django_dsg/settings.py
+++ django_dsg/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 #STATIC_URL = '/static/'
+STATIC_URL = 'frontend/build/static'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/build/static')
 ]
