d2f2355c430901307138b721b5b8f70655e144fc
kulriz <42681721+kulriz@users.noreply.github.com>
2020-10-13 04:53:42 -0400

    movd title to center and able to toggle btwn pages via '/'

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Navigation from './Navigation'
-
-
+import GameBoard from './GameBoard'
+import Home from './Home'
+import {Route, Link, Switch} from "react-router-dom";
+import { BrowserRouter as Router } from 'react-router-dom'
 function Header(){
     return(
-        <header className="border-dotted border-4 border-gray-600 font-bold p-5 flex justify-between ">
+        <header className="border-dotted border-4 border-gray-600 font-bold p-5 flex justify-center ">
         Data Structures Game
-        <Navigation />
+          
+    {/* <Navigation /> */}
         </header>
     )   
 }
