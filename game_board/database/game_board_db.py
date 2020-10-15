ddf569c1109575a8aead7fcea30260ec3e31e655
Ryan Barron <ryan.barron@saleswarp.com>
2020-10-15 00:50:33 -0400

    Added the docstrings for the database calls

--- game_board/database/game_board_db.py
+++ game_board/database/game_board_db.py
@@ -1,10 +1,24 @@
 from pymongo import MongoClient
 import os
 
+# Gets database & it's authorization from the environment
 DATABASE_URL1 = os.environ.get('DATABASE_URL1')
 client = MongoClient(DATABASE_URL1)
 
 def create_game(board):
+    """
+    Saves a new game into the database after being passed through the Django API
+
+    Parameters:
+    board (dictionary): cornicopia of game information such as current graph, players,
+                    turn data, and time data
+
+    Returns:
+    On Success:
+        str: unique identification for the game
+    On Fail:
+        str: friendly response to infrom of an error
+    """
     gameid = board["game_id"]
     user_list = board["player_ids"]
 
@@ -25,12 +39,38 @@ def create_game(board):
         return 'nah bro idk about it'
 
 def update_game(game_id: str, board):
+    """
+    Finds an active game in the Database and updates the values to reflect players'
+    in-game decisions
+
+    Parameters:
+    game_id (str): unique identification for the game
+    board (dictionary): cornicopia of game information such as current graph, players,
+                    turn data, and time data
+    Returns:
+    On Success:
+        dictionary: original game document
+    On Fail:
+        str: friendly response to infrom of an error
+    """
     value_returned = client.InitialDB.Active_Games.find_one_and_replace({"game_id": game_id}, board)
     if value_returned == None:
         return 'nah bro idk about it'
     return value_returned
 
 def read_game(game_id: str):
+    """
+    Allows an active game to be extracted from the database and passed back to the API
+
+    Parameters:
+    game_id (str): unique identification for the game
+
+    Returns:
+    On Success:
+        dictionary: an active game document
+    On Fail:
+        str: friendly response to infrom of an error
+    """
     DATABASE_URL1 = os.environ.get('DATABASE_URL1')
     client = MongoClient(DATABASE_URL1)
 
@@ -40,11 +80,31 @@ def read_game(game_id: str):
     return value_returned
 
 def remove_game(game_id: str):
+    """
+    Allows an active game to be located and then deleted from the database
 
+    Parameters:
+    game_id (str): unique identification for the game
+
+    Returns:
+    On Success:
+        int: number of deleted game documents
+    On Fail:
+        str: friendly response to infrom of an error
+    """
     value_returned = client.InitialDB.Active_Games.delete_one({"game_id": game_id}).acknowledged
     if value_returned == False:
         return 'nah bro idk about it'
     return value_returned
 
 def list_games():
+    """
+    Allows unique ids of all active games to be passed back to the API
+
+    Parameters:
+    None
+    
+    Returns:
+        cursor: to iterate game ids
+    """
     return client.InitialDB.Active_Games.find({},{'_id':0, 'game_id': 1})
