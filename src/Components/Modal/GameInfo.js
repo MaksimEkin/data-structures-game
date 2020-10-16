1c647ac170e4256ec512a6cd77d8e1a9e1215b4b
kulriz <42681721+kulriz@users.noreply.github.com>
2020-10-16 04:04:29 -0400

    added modal to homepage, figuring out how to get info from one component to another via ref

--- src/Components/Modal/GameInfo.js
+++ src/Components/Modal/GameInfo.js
@@ -5,43 +5,67 @@ import { render } from '@testing-library/react';
 class GameInfo extends React.Component {
     constructor(props) {
         super(props);
+        this.customNodeRef = React.createRef();
         //let hyparlink = props.hyparlink || new Hyparlink();
-        this.state = {level:null, playerList:null, gameDS:null};
-    }
+        this.state = {level:null, playerList:null, gameDS:null, game_id:null};
+        this.handleInput = this.handleInput.bind(this)
+    }   
+    handleInput = ({ target }) => {
+        this.setState({ [target.name]: target.value });
+        console.log(this.state)
+    };
+   getURLfxn =()=>
+    {
+        let levelU= this.state.level
+        let playerListU = this.state.playerList
+        let gameDSU = this.state.gameDS
+        
+        let start_game_url="/game_board/api/start_game/"+ {levelU}+"/"+{playerListU}+"/"+{gameDSU}
+        console.log(start_game_url)
+        fetch({start_game_url})
+        .then(response => response.json())
+        .then(data => this.setState({ game_id: data.total }));
+        console.log(this.state.game_id)
 
+        
+    }
+   
 render(){
+    this.getURLfxn()
     return(
         <Grid container spacing={1} style={{ marginBottom: '5em' }}>
             <Grid item sm={6} md={6} lg={6}>
                 <TextField 
                     required 
                     fullWidth
-                    id='level-input'
+                    name='level'
                     label='Difficulty Level' 
                     value={this.state.level} 
-                    onChange={e => this.handleInput('Hard, Medium, Easy', e.target.value)} 
+                    onChange={this.handleInput} 
                     style={{ marginBottom: '1em' }} 
                 />
                 <TextField 
                     required
                     fullWidth
-                    id='playerList-input' 
+                    name='playerList' 
                     label='players'
                     value={this.state.playerList} 
-                    onChange={e => this.handleInput('Players (comma separated)', e.target.value)} 
+                    onChange={this.handleInput} 
                     style={{ marginBottom: '1em' }} 
                 />
                 <TextField 
                     fullWidth 
                     multiline 
-                    id='DS-input' 
+                    name='gameDS' 
                     label='DSgame'
                     value={this.state.gameDS} 
-                    onChange={e => this.handleInput('AVL', e.target.value)} 
+                    onChange={this.handleInput} 
                     style={{ marginBottom: '1em' }} 
                 />            
             </Grid>
-         </Grid>
+            
+
+            </Grid>
                 
     );
 }
