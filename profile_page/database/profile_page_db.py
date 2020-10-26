b33f10ce12538460144175b85308b9b4ac3a6a88
Ryan Barron <ryanb4@umbc.edu>
2020-10-26 09:16:09 -0400

    added an aggregate statement to the save to make sure the number of games matching the new game id is less than 1 -- ie there will not be a duplicate if the new one is saved

--- profile_page/database/profile_page_db.py
+++ profile_page/database/profile_page_db.py
@@ -340,6 +340,34 @@ def load_board(user_id : str, user_game_id: str):
         except:
             return False
 
+def list_game_ids(user_id : str, game_id : str):
+    """
+    Finds an active user in the Database and returns a specific game to the API
+
+    Parameters:
+    user_id (str): unique identification for the user
+    game_index (int): locates which save game to overwrite
+
+    Returns:
+    On Success:
+        dictionary: original user game
+    On Fail:
+        Boolean: False
+    """
+
+
+    for item in client.InitialDB.User_Profile.aggregate([
+        {"$match":{"user_id": user_id}},
+        {"$unwind":"$save_games"},
+        {"$match":{"save_games.game_id": game_id}},
+        {"$count":"save_games"}
+    ]):
+        if (item['save_games'] >0):
+            print("True")
+        else:
+            print("False")
+    print("Continues")
+
 def save_game( user_id : str, board : dict ):
     """
     Finds an active user in the Database and appends a specific game to saves
@@ -354,6 +382,18 @@ def save_game( user_id : str, board : dict ):
     On Fail:
         Boolean: False
     """
+
+    # Returns a cursor -- only reason for loop (expecting {'save_games': 1} if game exists)
+    for item in client.InitialDB.User_Profile.aggregate([
+        {"$match":{"user_id": user_id}},
+        {"$unwind":"$save_games"},
+        {"$match":{"save_games.game_id": board["game_id"]}},
+        {"$count":"save_games"}
+    ]):
+        if (item['save_games'] > 0):
+            return False
+
+    # Pushes the game to user's save game list
     value_returned = client.InitialDB.User_Profile.update_one(
         {"user_id": user_id},
         {"$push":{"save_games":board}},
