9e4d5fcb50809b86a5775dbbec6247876685f5cc
kulriz <42681721+kulriz@users.noreply.github.com>
2020-10-29 23:22:27 -0400

    ui changes to nav bar, added routing to login, start game and home

--- src/App.js
+++ src/App.js
@@ -5,24 +5,13 @@ import Home from './Components/Home'
 import Header from './Components/Header';
 import {Route, Link, Switch} from "react-router-dom";
 import { BrowserRouter as Router } from 'react-router-dom'
-import Navigation from './Components/Navigation'
- 
+//react router used to make this web app multiple pages instead of single page application
 function App(){
   return (
     <div>      
     <Router>
     <Header />
-      <ul>
-        <li>
-          <Link className="text-blue-500 py-3 border-t bordre-b block" to="/" >
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link className="text-blue-500 py-3 border-t bordre-b block" to="/game_board" name="start_game">
-            Start Game</Link>
-        </li>
-      </ul>
+     
       <Switch>
         <Route exact path="/">
           <Home/>
