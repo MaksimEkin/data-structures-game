3f649ea07949ccf4eb8ab7e750cdbd5a10ce8ad7
kulriz <42681721+kulriz@users.noreply.github.com>
2020-10-30 01:46:13 -0400

    removed css that is not needed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -7,10 +7,13 @@ import NavMenu from './NavMenu';
 //header component called in every page and displays its child component to display the menu
 function Header(){
     return(
-        <header className="border-b p-3 flex justify-between items-center">
+<header className="border-b p-3 flex justify-between items-center">
         Data Structures Game
+
     <NavMenu />
         </header>
+
+        
     )   
 }
 export default Header
