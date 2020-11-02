f2e0439127c0331c722e6e99eb55417c321f03bc
Naomi <albert10@umbc.edu>
2020-11-01 20:35:23 -0500

    Display winner & their points, removed buggy code for displaying other players' points

--- src/Components/Modal/WinModal.js
+++ src/Components/Modal/WinModal.js
@@ -17,23 +17,22 @@ class WinModal extends Component {
     }
 
     render() {
-        var i
+        /*var i
         let players = []
         for (i = 0; i < this.state.board["num_players"]; i++) {
-            console.log(this.state.board["player_ids"][i])
             players.push(this.state.board["player_ids"][i])
         }
 
         let testing = ""
         for (i = 0; i < this.state.board["num_players"]; i++){
-            testing += this.state.board["player_ids"][i]
+            testing += players[i]
             testing += this.state.board["player_points"][[players][i]]
         }
 
-        console.log("Testing: ", testing)
+        console.log("Testing: ", testing) */
 
 
-        console.log("Winner: ", this.state.winner)
+        //Display win message
         Swal.fire({
             title: "Congrats! You are the Data Structures Wizard!",
             text: "Winner is: " + this.state.winner + " with " + this.state.board["player_points"][this.state.winner] + " points!",
