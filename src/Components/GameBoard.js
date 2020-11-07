56fe60e716212285d7e7523962df91567afa28cf
kulriz <42681721+kulriz@users.noreply.github.com>
2020-11-07 01:08:17 -0500

    added cookie values in constructor

--- src/Components/GameBoard.js
+++ src/Components/GameBoard.js
@@ -72,7 +72,7 @@ class GameBoard extends Component {
       //used in conjunction with the API's end_game returned in the JSON
       game_over: false
     };
-    //set intial cookie values in constructor for game info
+    
     
   }
 
