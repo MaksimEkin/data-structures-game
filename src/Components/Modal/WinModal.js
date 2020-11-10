d5bb230d7adb3cad26fb1410bd97d44391e83cef
Naomi <albert10@umbc.edu>
2020-11-09 22:23:04 -0500

    Removed trailing comma from win modal text

--- src/Components/Modal/WinModal.js
+++ src/Components/Modal/WinModal.js
@@ -19,52 +19,37 @@ class WinModal extends Component {
     render() {
 
         var i
-        let players = []
+
+        //string to store other players' scores
         let playerScores = ""
+
+        //loop through all players in board
         for (i = 0; i < this.state.board["num_players"]; i++){
+
+            //don't include winner in the list of other players (their score displayed separately)
             if (!(this.state.board["player_ids"][i] == this.state.winner)){
+
+                //get id of player (not a winner)
                 let curPlayer =  this.state.board["player_ids"][i]
                 playerScores += curPlayer
-                playerScores += " with " + this.state.board.player_points[curPlayer] + " points, \n"
-            }
-        }
-        console.log("PlayerScores: ", playerScores)
-        /*console.log("In Win Modal, printing board", this.state.board)
-        var i
-        let players = []
-        for (i = 0; i < this.state.board["num_players"]; i++) {
-            console.log(this.state.board["player_ids"][i])
-            console.log("winner", this.state.winner)
-            if (!(this.state.board["player_ids"][i] == this.state.winner)) {
-                console.log("In if")
-                players.push(this.state.board["player_ids"][i])
+
+                //and their score
+                playerScores += " with " + this.state.board.player_points[curPlayer] + " points!,"
             }
         }
 
-        var j
-        let playerList = ""
-        for (j = 0; j < players.length; j++){
-            playerList += players[j] + "\n"
-        }
-
-        console.log("PlayerList, ", playerList)
-
-        console.log("Player points", this.state.board.player_points[players[0]]);
-/*
-        let testing = ""
-        for (i = 0; i < this.state.board["num_players"]; i++){
-            testing += players[i]
-            testing += this.state.board["player_points"][[players][i]]
-        }
-
-        console.log("Testing: ", testing) */
-
+        //slice off last comma
+        playerScores = playerScores.substring(0, playerScores.length - 1)
 
         //Display win message
         Swal.fire({
+            //display winner score
             title: "Congrats " + this.state.winner + "! You are a Data Structures Wizard, with "
                 + this.state.board["player_points"][this.state.winner] + " points!",
+
+            //display other players' scores
             text: "Other all-stars: " + playerScores,
+
             imageUrl: Party_Face,
             confirmButtonText: "Return to Home Page"
         }).then((result) => {
