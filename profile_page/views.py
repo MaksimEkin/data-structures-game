25cf2066385514959c60c87a624e90875b9b49f6
Naomi <albert10@umbc.edu>
2020-11-11 17:55:24 -0500

    Add Front-end & Django routes for Profile Page/Login

--- profile_page/views.py
+++ profile_page/views.py
@@ -4,3 +4,10 @@
 
 #from django.shortcuts import render
 # Create your views here.
+from django.shortcuts import render
+
+def profile_page(request):
+    """Redirect to the game board view."""
+
+    # Change this to the actual React frontend for game board when ready.
+    return render(request, 'index.html')
