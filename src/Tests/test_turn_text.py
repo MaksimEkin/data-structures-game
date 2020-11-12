63edc753a4b707defc900f882a179c50fd3cf6c7
MaksimEkin <meren1@umbc.edu>
2020-11-12 03:56:48 -0500

    Fix Pylint complience.

--- src/Tests/test_turn_text.py
+++ src/Tests/test_turn_text.py
@@ -18,7 +18,6 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import Select
-from selenium.webdriver.common.by import By
 
 
 class TestStringMethods(unittest.TestCase):
