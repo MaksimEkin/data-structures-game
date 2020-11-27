20d09aa18717f4cc98d1f2f99384d5a0c5736a30
Ryan Barron <ryanb4@umbc.edu>
2020-11-26 19:43:08 -0500

    function and test added to clean the database

---
+++ game_board/database/purge_old_games.py
@@ -0,0 +1,11 @@
+from game_board_db import *
+
+if __name__ == '__main__':
+
+    purge_old_games()
+
+"""
+Big red button code- Dont use
+    #for game in list_games():
+       #remove_game(game['game_id'])
+"""
