c980d8b7cc7f8fdc48bc53ae052ba6ac0dd4c3c5
Nick <sonic1@umbc.edu>
2020-11-29 01:16:20 -0500

    Got the table to build but without any tailwind styling

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -8,7 +8,6 @@ import Particles from "react-particles-js"
 const local = "http://127.0.0.1:8000/";
 const reactLocal = "http://localhost:3000/"
 const remote = "https://data-structures-game.herokuapp.com/";
-const gamesDiv = document.querySelector("div.games") // Find the game table div in source
 const tableHeaders = ["Game", "Type", "Difficulty", "Actions"]
 
 //can also be const url = local; or const url = reactLocal;
@@ -37,8 +36,11 @@ class Profile extends Component {
             username: null,
             password: null,
             loggedIn: prevLogin,
-            userPoints: null,
-            userRank: null
+            user_name: null,
+            user_points: null,
+            user_rank: null,
+            user_games: null,
+            is_loaded: false
         }
     }
     
@@ -46,15 +48,11 @@ class Profile extends Component {
         
         console.log("in componentDidMount()");
 
-        //display login screen if not logged in
+        //call the profile api call when user is logged in
         const cookies = new Cookies()
-        if ((this.state.loggedIn === false) || (cookies.get('token') === '')){
-            return (this.displayLogIn())
-        }
-        //display user profile page with option to log out
-        else {
+        if (!(this.state.loggedIn === false) && !(cookies.get('token') === '')){
             this.profileAPICall()
-            return(this.displayUserProfile())
+            this.setState({is_loaded:true})
         }
     }
 
@@ -73,6 +71,7 @@ class Profile extends Component {
 
         //use username as salt for SHA-256 hash, so combine username and plaintext pw into one string
         let toHash = this.state.username + this.state.password
+        this.setState({password: ""})
 
         //hash it and convert to string format
         let hashed = CryptoJS.SHA256(toHash)
@@ -272,6 +271,113 @@ class Profile extends Component {
             </div></span></div></div></div>
         )
     }
+   
+    viewCallback = (id) => {
+        /* Here we are going to call the view API call. This is a dummy callback for now as viewing will not make any change to user profile.
+            Need to have the user_id & token when on the real profile page */
+        console.log("viewCallback for id: " + id);
+        //api call to load game here
+    }
+    
+    shareCallback = (id) => {
+        /* Here we are going to call the view API call. This is a dummy callback for now as viewing will not make any change to user profile.
+            Need to have the user_id & token when on the real profile page */
+        console.log("shareCallback for id: " + id);
+        // Swal.fire({
+        //     title: 'Submit your Github username',
+        //     input: 'text',
+        //     inputAttributes: {
+        //       autocapitalize: 'off'
+        //     },
+        //     showCancelButton: true,
+        //     confirmButtonText: 'Look up',
+        //     showLoaderOnConfirm: true,
+        //     preConfirm: (login) => {
+        //       return fetch(`//api.github.com/users/${login}`)
+        //         .then(response => {
+        //           if (!response.ok) {
+        //             throw new Error(response.statusText)
+        //           }
+        //           return response.json()
+        //         })
+        //         .catch(error => {
+        //           Swal.showValidationMessage(
+        //             `Request failed: ${error}`
+        //           )
+        //         })
+        //     },
+        //     allowOutsideClick: () => !Swal.isLoading()
+        //   }).then((result) => {
+        //     if (result.isConfirmed) {
+        //       Swal.fire({
+        //         title: `${result.value.login}'s avatar`,
+        //         imageUrl: result.value.avatar_url
+        //       })
+        //     }
+        //   })
+    }
+    
+    deleteCallback = (id) => {
+        /* Here we are going to call the delete API call. For now this just removes .
+            Need to have the user_id & token when on the real profile page */
+        console.log("deleteCallback for id: " + id);
+    }
+    
+    // The function below will accept a single game and its index to add it to the table
+    appendGame = (singleGame) => {
+        console.log("appendGames");
+
+        let blueButtonStyle = "text-grey-lighter font-bold py-1 px-3 mx-1 rounded text-xs bg-blue hover:bg-blue-dark"
+        let redButtonStyle = "text-grey-lighter font-bold py-1 px-3 mx-1 space-x-4 rounded text-xs bg-red hover:bg-red-dark"
+
+        return( 
+            <tr className="gamesTableBodyRow"> 
+                <td className="py-4 px-6 border-b border-grey-light"> {singleGame.game_id.substring(0,8)}</td>
+                <td className="py-4 px-6 border-b border-grey-light"> {singleGame.curr_data_structure}</td>
+                <td className="py-4 px-6 border-b border-grey-light"> {singleGame.difficulty}</td>
+                <td className="flex space-x-4 py-4 px-6 border-b border-grey-light">
+                    <button className={blueButtonStyle} onClick={() => this.viewCallback(singleGame.game_id)}> View </button>
+                    <button className={blueButtonStyle} onClick={() => this.shareCallback(singleGame.game_id)}> Share </button>
+                    <button className={redButtonStyle} onClick={() => this.deleteCallback(singleGame.game_id)}> Delete </button>
+                </td>               
+            </tr>
+        )
+    }
+    
+
+    buildGamesHeader = (col_name) => {
+        return(<th className="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-b border-grey-light">
+                    {col_name}
+               </th>)
+    }
+
+    buildGames = () => {
+        console.log("buildGames");
+        let games = this.state.user_games;
+        if(games == null || games.length == 0) {
+            return <div></div>;
+        } else {
+
+            let header = []
+            tableHeaders.forEach(header_col => {
+                header.push(this.buildGamesHeader(header_col));
+            })
+
+            let game_rows = []
+            for (const game of games) {
+                game_rows.push(this.appendGame(game))  // Creates and appends each row to the table body
+            }
+
+            return(
+                <div>
+                  <table className="text-left w-full border-collapse">
+                    <thead className="gamesTableHead"> {header} </thead>
+                    <tbody className="gamesTableBody"> {game_rows} </tbody>
+                  </table>
+                </div>
+            )
+        }
+    }
 
     //display user profile and sign out button
     displayUserProfile = () => {
@@ -286,29 +392,26 @@ class Profile extends Component {
                                 <img src="/static/bohemian_panda.png" class="rounded-full border-solid border-white border-2 -mt-3 h-32 w-32" />
                             </div>
                             <div class="text-center px-3 pb-6 pt-2">
-                                <h3 class="text-black text-sm bold font-sans">@username</h3>
+                                <h3 class="text-black text-sm bold font-sans">{this.state.is_loaded && this.state.user_name}</h3>
                             </div>
                             <div class="flex justify-center pb-3 text-grey-dark">
                                 <div class="text-center mr-3 border-r pr-3">
-                                    <h2>@total_points</h2>
+                                    <h2>{this.state.is_loaded && this.state.user_points}</h2>
                                     <span>Total Points</span>
                                 </div>
                                 <div class="text-center">
-                                    <h2>#@ranking</h2>
+                                    <h2>{this.state.is_loaded && this.state.user_rank}</h2>
                                     <span>Ranking</span>
                                 </div>
                             </div>
                         </div>
-
                         <div class="bg-white shadow-md rounded my-6">
-                            <div class='games'></div>
+                            {this.state.is_loaded && this.buildGames()}
                         </div>
                     </div>
                 </div>
             </div>)
     }
-
-
 
 
     //api call to get user's rank and points
@@ -344,8 +447,10 @@ class Profile extends Component {
 
         //otherwise, jsonify the api return
         let returned = await response.json()
-        this.setState({ user_points: returned["user_profile"]["points"],
-                             user_rank: returned["user_profile"]["rank"]})
+        this.setState({ user_name: returned["user_profile"]["user_name"],
+                        user_points: returned["user_profile"]["points"],
+                        user_rank: returned["user_profile"]["rank"],
+                        user_games: returned["user_profile"]["saved_games"]})
     }
 
 
@@ -359,7 +464,17 @@ class Profile extends Component {
 
         //display user profile page with option to log out
         else {
-            return(this.displayUserProfile())
+            if (this.state.is_loaded) {
+                return(this.displayUserProfile())
+            } else {
+                return(
+                     <div class="w-full h-full fixed block top-0 left-0 bg-white opacity-75 z-50">
+                       <span class="text-green-500 opacity-75 top-1/2 my-0 mx-auto block relative w-0 h-0">
+                        <i class="fas fa-circle-notch fa-spin fa-5x"></i>
+                      </span>
+                    </div>
+                )
+            }
         }
     }
 }
