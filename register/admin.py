8818fdc1e21754d40e60623f2af3be55be39a6eb
MaksimEkin <meren1@umbc.edu>
2020-11-29 18:35:48 -0500

    pylint fix.

--- register/admin.py
+++ register/admin.py
@@ -1,3 +1,7 @@
-from django.contrib import admin
+"""
+    Views for Register Page application.
+"""
+
+#from django.contrib import admin
 
 # Register your models here.
--- register/tests.py
+++ register/tests.py
@@ -1,3 +1,6 @@
-from django.test import TestCase
+"""
+    Views for Register Page application.
+"""
+#from django.test import TestCase
 
 # Create your tests here.
