8d5248d19be08c1fd1155f8a222d99a3e2576d5e
Ryan Barron <ryanb4@umbc.edu>
2020-11-29 14:11:22 -0500

    pylint issues

--- game_board/database/purge_old_games.py
+++ game_board/database/purge_old_games.py
@@ -1,8 +1,8 @@
+"""
+Removes games older than a day from the mongo db
+command to run: python game_board/database/purge_old_games.py
+"""
 from game_board_db import purge_old_games
 
 if __name__ == '__main__':
-    """
-    Removes games older than a day from the mongo db
-    """
-
     purge_old_games()
