193447e09054bef18be31e7900f4d9cba53f9dc6
kulriz <42681721+kulriz@users.noreply.github.com>
2020-11-30 21:06:47 -0500

    adding files

--- src/Components/NavMenu.js
+++ src/Components/NavMenu.js
@@ -15,12 +15,12 @@ function NavMenu(){
     let menu
     let menuMask
     if(showMenu){
-        menu= 
+        menu=
         <div
         className="fixed bg-gray-200 top-0 right-0 w-1/6 h-3/5 z-50 shadow-2xl rounded-2xl"
-        > 
+        >
         <div class="dropdown">
-  
+
         <ul class='nav-links'>
                 <li>
                     <a className="space-y-20 text-lg py-3 border-t border-b block text-center font-semibold text-gray-800 mb-2" href='/'>Home</a>
@@ -28,19 +28,22 @@ function NavMenu(){
                 <li>
                     <a className="space-y-20 text-lg py-3 border-t border-b block text-center font-semibold text-gray-800 mb-2" href='/profile_page'>Profile & Login</a>
                 </li>
+                <li>
+                    <a className="space-y-20 text-lg py-3 border-t border-b block text-center font-semibold text-gray-800 mb-2" href='/register'>Register</a>
+                </li>
         </ul>
         </div>
         <div class="col-lg-6 col-md-6" style={{'align-self':'flex-end'}}>
             <img src={panda} alt={"panda"} />
             <a href="https://www.vecteezy.com/free-vector/material">Material Vectors by Vecteezy</a>
         </div>
-        
+
         </div>
-        
+
         menuMask=
         <div className="bg-black-t-50 fixed top-0 left-0 w-full h-full z-50"
             onClick={() => setShowMenu(false)}
-        >  
+        >
 
         </div>
     }
@@ -48,12 +51,12 @@ function NavMenu(){
     return(
         <header>
         <nav>
-           
-                <FontAwesomeIcon 
+
+                <FontAwesomeIcon
                 icon={faBars} size="lg"
                 onClick={() => setShowMenu(!showMenu)}
                 />
-            
+
             {menuMask}
             {menu}
         </nav>
@@ -61,5 +64,3 @@ function NavMenu(){
     )
 }
 export default NavMenu
-
-
