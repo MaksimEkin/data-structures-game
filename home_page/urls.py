6157a6b9706c293114a3318ab5a44db106f3563b
MaksimEkin <meren1@umbc.edu>
2020-12-07 19:16:43 -0500

    added the new api call to get top n players.

--- home_page/urls.py
+++ home_page/urls.py
@@ -3,7 +3,12 @@
 """
 from django.urls import path
 from . import views
+from home_page.api import api
 
 urlpatterns = [
     path('', views.home, name='home-page'),
+
+    # Homepage API call to get the ranking
+    path('api', api.api_overview, name='homepage-api_overview'),
+    path('api/rankings/<int:top_n>', api.rankings, name='game-board-ranking-top-n'),
 ]
